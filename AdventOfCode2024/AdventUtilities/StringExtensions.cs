3ef1823310c80de75ed7cd7bcaeb7fda792da7ed
Phil Pursglove <philpursglove@users.noreply.github.com>
2024-12-04 22:36:34 +0000

    WordSearch library method

--- AdventOfCode2024/AdventUtilities/StringExtensions.cs
+++ AdventOfCode2024/AdventUtilities/StringExtensions.cs
@@ -17,6 +17,115 @@
             return grid;
         }
 
+        public static List<Tuple<Coordinate, Coordinate>> Search(this string[,] grid, string searchText)
+        {
+            string reversedSearchText = string.Join("", searchText.Reverse());
+
+            List<Tuple<Coordinate, Coordinate>> searchResults = new List<Tuple<Coordinate, Coordinate>>();
+
+            for (int y = 0; y < grid.GetUpperBound(1) + 1; y++)
+            {
+                string row = string.Empty;
+                for (int x = 0; x < grid.GetUpperBound(0) + 1; x++)
+                {
+                    row += grid[x, y];
+                }
+
+                if (row.Contains(searchText, StringComparison.CurrentCulture))
+                {
+                    int startPosition = 0;
+
+                    while (row.IndexOf(searchText, startPosition, StringComparison.CurrentCulture) != -1)
+                    {
+                        startPosition = row.IndexOf(searchText, startPosition, StringComparison.CurrentCulture) + 1;
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(startPosition, y),
+                            new Coordinate(startPosition + (searchText.Length - 1), y)));
+
+                    }
+                }
+                if (row.Contains(reversedSearchText, StringComparison.CurrentCulture))
+                {
+                    int startPosition = 0;
+
+                    while (row.IndexOf(reversedSearchText, startPosition, StringComparison.CurrentCulture) != -1)
+                    {
+                        startPosition = row.IndexOf(reversedSearchText, startPosition, StringComparison.CurrentCulture) + 1;
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(startPosition, y),
+                            new Coordinate(startPosition + (searchText.Length - 1), y)));
+
+                    }
+                }
+            }
+
+            for (int x = 0; x < grid.GetUpperBound(0) + 1; x++)
+            {
+                string col = string.Empty;
+                for (int y = 0; y < grid.GetUpperBound(1) + 1; y++)
+                {
+                    col += grid[x, y];
+                }
+
+                if (col.Contains(searchText, StringComparison.CurrentCulture))
+                {
+                    int startPosition = 0;
+
+                    while (col.IndexOf(searchText, startPosition, StringComparison.CurrentCulture) != -1)
+                    {
+                        startPosition = col.IndexOf(searchText, startPosition, StringComparison.CurrentCulture) + 1;
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(x, startPosition),
+                            new Coordinate(x, startPosition + (searchText.Length - 1))));
+
+                    }
+                }
+                if (col.Contains(reversedSearchText, StringComparison.CurrentCulture))
+                {
+                    int startPosition = 0;
+
+                    while (col.IndexOf(reversedSearchText, startPosition, StringComparison.CurrentCulture) != -1)
+                    {
+                        startPosition = col.IndexOf(reversedSearchText, startPosition, StringComparison.CurrentCulture) + 1;
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(x, startPosition),
+                            new Coordinate(x, startPosition + (searchText.Length - 1))));
+                    }
+                }
+            }
+
+            for (int y = 0; y < grid.GetUpperBound(1) - (searchText.Length - 2); y++)
+            {
+                for (int x = 0; x < grid.GetUpperBound(0) - (searchText.Length - 2); x++)
+                {
+                    string diagonal = string.Empty;
+                    for (int position = 0; position < searchText.Length; position++)
+                    {
+                        diagonal += grid[x + position, y + position];
+                    }
+                    if (diagonal == searchText || diagonal == reversedSearchText)
+                    {
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(x, y), new Coordinate(x + searchText.Length - 1, y + searchText.Length - 1)));
+                    }
+                }
+            }
+
+            for (int y = 0; y < grid.GetUpperBound(1) - (searchText.Length - 2); y++)
+            {
+                for (int x = grid.GetUpperBound(0); x > (searchText.Length - 2); x--)
+                {
+                    string diagonal = string.Empty;
+                    for (int position = 0; position < searchText.Length; position++)
+                    {
+                        diagonal += grid[x - position, y + position];
+                    }
+                    if (diagonal == searchText || diagonal == reversedSearchText)
+                    {
+                        searchResults.Add(new Tuple<Coordinate, Coordinate>(new Coordinate(x, y), new Coordinate(x - (searchText.Length - 1), y + searchText.Length - 1)));
+                    }
+                }
+
+            }
+
+            return searchResults;
+        }
+
         public static int[,] ToIntGrid(this string[] strings)
         {
             int[,] grid = new int[strings.First().Length, strings.Length];
